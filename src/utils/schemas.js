28fd972189d19482c65617ae6e417a468c60fa92
Inna <innacergeevna501@gmail.com>
2024-10-22 16:23:38 +0300

    2

--- src/utils/schemas.js
+++ src/utils/schemas.js
@@ -5,7 +5,5 @@ export const addContactSchema = Yup.object({
     .min(3, "Name must be at least 3 characters")
     .max(50, "Name cannot exceed 50 characters")
     .required("This field is required"),
-  number: Yup.string()
-    .required("This field is required")
-    .matches(/^\+?\d{1,3}[-. ]?\d{1,4}[-. ]?\d{1,4}[-. ]?\d{1,9}$/),
+  number: Yup.string().required("This field is required"),
 });
